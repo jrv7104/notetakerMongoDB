9def741c61bc74edc120ec43f826369025ab4c68
Jason Viox <jrv04@hotmail.com>
2021-08-31 21:21:22 -0500

    Add functionality to create and get by id

--- app/contoller.js
+++ app/contoller.js
@@ -1,1 +1,19 @@
 // Uses the db client from loader.js
+import { ObjectId } from "mongodb";
+import client from "./loader.js";
+
+const notesConnection = client.db("noteTaker").collection("notes");
+
+export default {
+  index() {
+    return notesConnection.find({}).toArray();
+  },
+
+  create(newNote) {
+    return notesConnection.insertOne(newNote);
+  },
+
+  show(id) {
+    return notesConnection.findOne(ObjectId(id));
+  },
+};
