201c1f473ab24ee0d9a6211c449df29849ed8cf0
Jason Viox <jrv04@hotmail.com>
2021-08-31 21:21:33 -0500

    Add functionality

--- app/router.js
+++ app/router.js
@@ -1,4 +1,5 @@
 import Router from "express";
+import noteController from "./contoller.js";
 
 const router = new Router();
 
@@ -7,8 +8,30 @@ router.get("/", (_, res) => {
   res.send("Hello World from API");
 });
 
-router.post("/", (req, res) => {
-  console.log(req.body);
+router.get("/notes", async (_, res) => {
+  try {
+  const notes = await noteController.index();
+  res.json(notes);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.get("/notes/:id", async (req, res) => {
+  try {
+    const note = await noteController.show(req.params.id);
+    res.json(note)
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.post("/notes", async (req, res) => {
+  const newNote = await noteController.create(req.body);
+  res.status(201).json(newNote);
+} catch (error) {
+  res.status(400).json(error.message);
+}
 });
 
 export default router;
