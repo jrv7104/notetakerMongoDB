5701ae7da075fa86d634c451ef25ecde09966a75
Jason Viox <jrv04@hotmail.com>
2021-09-02 19:46:47 -0500

    Update functions

--- app/router.js
+++ app/router.js
@@ -1,5 +1,5 @@
 import Router from "express";
-import noteController from "./contoller.js";
+import noteController from "./controller.js";
 
 const router = new Router();
 
@@ -10,8 +10,8 @@ router.get("/", (_, res) => {
 
 router.get("/notes", async (_, res) => {
   try {
-  const notes = await noteController.index();
-  res.json(notes);
+    const notes = await noteController.index();
+    res.json(notes);
   } catch (error) {
     res.status(400).send(error);
   }
@@ -20,24 +20,43 @@ router.get("/notes", async (_, res) => {
 router.get("/notes/:id", async (req, res) => {
   try {
     const note = await noteController.show(req.params.id);
-    res.json(note)
+    res.json(note);
   } catch (error) {
     res.status(400).send(error);
   }
 });
 
 router.post("/notes", async (req, res) => {
-  const newNote = await noteController.create(req.body);
-  res.status(201).json(newNote);
-} catch (error) {
-  res.status(400).json(error.message);
-}
+  try {
+    const newNote = await noteController.create(req.body);
+    res.status(201).json(newNote);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+});
+
+router.put("/notes/:id", async (req, res) => {
+  try {
+    const updatedNote = await noteController.update(req.params.id, req.body);
+    res.json(updatedNote);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 router.delete("/notes/:id", async (req, res) => {
   try {
     await noteController.delete(req.params.id);
     res.status(204);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.delete("notes", async (req, res) => {
+  try {
+    await noteController.deleteAll();
+    res.status(204).send();
   } catch (error) {
     res.status(400).send(error);
   }
